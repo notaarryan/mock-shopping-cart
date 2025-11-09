009bcb8913c1ab55cab2a24e0f2ced836ff2e412
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2025-11-08 19:33:26 -0500

    feat: completed implementation of home page in Home.jsx and also implemented nav bar inside of App.jsx and styled them in respectice css modules

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,31 @@
 import { useState, useEffect } from "react";
 import { Outlet, Link } from "react-router";
+import styles from "./App.module.css";
 
-function App() {}
+function App() {
+  return (
+    <>
+      <div id={styles.main}>
+        <nav>
+          <div>Mock Shopping Cart</div>
+          <ul>
+            <button>
+              <Link to="/">Home</Link>
+            </button>
+            <button>
+              <Link to="/shop">Shop</Link>
+            </button>
+            <button>
+              <Link to="/cart">Cart</Link>
+            </button>
+          </ul>
+        </nav>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+    </>
+  );
+}
 
 export default App;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,16 @@
-function Home() {}
+import styles from "./styles/Home.module.css";
+
+function Home() {
+  return (
+    <section>
+      <div className={styles.title}>Welcome to the store</div>
+      <div className={styles.content}>
+        This is a basic project to display my skills in React.js, this project
+        showcases my abilities in using css modules, react-router, and fetching
+        data from api's using useEffect.
+      </div>
+    </section>
+  );
+}
 
 export default Home;
