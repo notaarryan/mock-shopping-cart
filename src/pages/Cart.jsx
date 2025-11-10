3771d3734dee045a1d0fd700348c57ce52fc67fb
Aryan Parmar <156536547+notaarryan@users.noreply.github.com>
2025-11-10 11:52:56 -0500

    feat: added cart is empty message

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -15,25 +15,27 @@ function Cart() {
     <section className={styles.shopSection}>
       <div className={styles.title}>Your Cart</div>
       <div className={styles.content}>
-        {Object.keys(shoppingItems).map((key) => {
-          if (idsOfItemsInCart.includes(key)) {
-            return (
+        {numberOfCartItems === 0 ? (
+          <div className={styles.emptyCart}>Your cart is empty 🛒</div>
+        ) : (
+          Object.keys(shoppingItems)
+            .filter((key) => idsOfItemsInCart.includes(key))
+            .map((key) => (
               <ItemCard
                 id={key}
+                key={key}
                 title={shoppingItems[key].title}
                 price={shoppingItems[key].price}
                 rating={shoppingItems[key].rating}
                 description={shoppingItems[key].description}
                 src={shoppingItems[key].image}
-                key={key}
                 setNumberOfCartItems={setNumberOfCartItems}
                 numberOfCartItems={numberOfCartItems}
                 idsOfItemsInCart={idsOfItemsInCart}
                 setIdsOfItemsInCart={setIdsOfItemsInCart}
               />
-            );
-          }
-        })}
+            ))
+        )}
       </div>
     </section>
   );
